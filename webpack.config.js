9723cdd75986f3dbf6d9dc5d36fe3b01f210fb1c
Jonathan Esquivel <Jonathane_30191@hotmail.com>
2024-06-28 22:49:56 -0600

    react

--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-crypto.createHash = (algorithm) => crypto.createHash(algorithm === 'md4' ? 'sha256' : algorithm);
-
-module.exports = function override(config, env) {
-  return config;
-};
