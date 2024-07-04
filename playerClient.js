3195339086a86341c4481862b80d6a43347c4a7b
Jonathan Esquivel <Jonathane_30191@hotmail.com>
2024-07-04 04:29:41 -0600

    SocketAuth

--- playerClient.js
+++ playerClient.js
@@ -2,82 +2,89 @@ let info = {};
 const backendUrl = 'https://rngbj.kasoom.com';
 const backendUrl2 = 'https://rngbj.kasoom.com';
 let Gtoken = '';
+const socket;
 
 console.log("Backend URL: ", backendUrl)
 
 //------------------------------------------------------------------------------------------------------------------Socket.on
 
-const socket = io(backendUrl, {
-    withCredentials: true,
-    upgrade: false,
-    transports: ['websocket']
-});
-
-socket.on('disconnect', (reason) => {
-    console.log("Disconnected: ", reason);
-    if (reason === "io server disconnect") {
-      // Server disconnected the socket
-      console.log("Server initiated disconnection.");
-      tryReconnect(new Date());
-    } else if (reason === "transport close" || reason === "ping timeout") {
-      // Network issues or client-side disconnects
-      console.log("Client-side disconnection or network issue.");
-      tryReconnect(new Date());
-    } else {
-      console.log("Unknown disconnection reason:", reason);
-    }
-})
-
-socket.on('newState', data => {
-    console.log("NewState Called...");
-    data.newStatus = true;
-    data.type = 'newState';
-    console.log(data);
-    postMessage(data, "*");
-})
-
-socket.on('newClientStatus', data => {
-    console.log('newClientStatus', data)
-    data.newStatus = true;
-    data.newPrivatestat = true;
-    postMessage(data, "*");
-})
-
-socket.on('NewBalance', () => {
-    console.log('New balance function called!')
-})
-
-socket.on("showMess", data => {
-    console.log("Posting message: ", data)
-    data.type = "showMess";
-    postMessage(data, '*')
-})
-
-/*
-socket.on('message', (message) => {
-    console.log('Message received :', message);
-    // Handle incoming messages from the WebSocket server
-    if (typeof message === 'string') {
-        const data = JSON.parse(message);
-        if (data.action === 'init_response') {
-            setUser(data.info.userId);
-            setPlayer(data.info.sessionId);
+function CreateSocket()
+{
+    socket = io(backendUrl, {
+        withCredentials: true,
+        upgrade: false,
+        transports: ['websocket'],
+        auth: {
+            token: Gtoken
         }
-    }
-});
-*/
+    });
 
-socket.on('connect_error', (error) => {
-    console.log("Connection error:", error.message);
-});
+    socket.on('disconnect', (reason) => {
+        console.log("Disconnected: ", reason);
+        if (reason === "io server disconnect") {
+            // Server disconnected the socket
+            console.log("Server initiated disconnection.");
+            tryReconnect(new Date());
+        } else if (reason === "transport close" || reason === "ping timeout") {
+            // Network issues or client-side disconnects
+            console.log("Client-side disconnection or network issue.");
+            tryReconnect(new Date());
+        } else {
+            console.log("Unknown disconnection reason:", reason);
+        }
+    })
 
-socket.on('error', (error) => {
-    console.error('WebSocket Error:', error);
-});
+    socket.on('newState', data => {
+        console.log("NewState Called...");
+        data.newStatus = true;
+        data.type = 'newState';
+        console.log(data);
+        postMessage(data, "*");
+    })
 
-socket.on('reconnect_error', (error) => {
-    console.log("Reconnection error:", error.message);
-});
+    socket.on('newClientStatus', data => {
+        console.log('newClientStatus', data)
+        data.newStatus = true;
+        data.newPrivatestat = true;
+        postMessage(data, "*");
+    })
+
+    socket.on('NewBalance', () => {
+        console.log('New balance function called!')
+    })
+
+    socket.on("showMess", data => {
+        console.log("Posting message: ", data)
+        data.type = "showMess";
+        postMessage(data, '*')
+    })
+
+    /*
+    socket.on('message', (message) => {
+        console.log('Message received :', message);
+        // Handle incoming messages from the WebSocket server
+        if (typeof message === 'string') {
+            const data = JSON.parse(message);
+            if (data.action === 'init_response') {
+                setUser(data.info.userId);
+                setPlayer(data.info.sessionId);
+            }
+        }
+    });
+    */
+
+    socket.on('connect_error', (error) => {
+        console.log("Connection error:", error.message);
+    });
+
+    socket.on('error', (error) => {
+        console.error('WebSocket Error:', error);
+    });
+
+    socket.on('reconnect_error', (error) => {
+        console.log("Reconnection error:", error.message);
+    });
+}
 
 //------------------------------------------------------------------------------------------------------------------Socket Functions
 
@@ -141,8 +148,8 @@ async function activateGame(token, operator = '') {
     Gtoken = finalRes.token;
     console.log("Token received: ", Gtoken)
     console.log("After init: ", info.tableID, info.playerID, Gtoken)
-    setPlayer('293', Gtoken)
-
+    setPlayer('528', Gtoken)
+    CreateSocket()
     if (finalRes.status == "OK") {
         //setPlayer();
     }
